3feac1865e2ce809c87c84b7bedc1d923677609e
Rohitkumar Murali Arasanipalai <rohitkumar97@gmail.com>
2024-04-01 13:39:05 -0700

    Added CommonSenseQA reader

--- commonSenseQAReader.py
+++ commonSenseQAReader.py
@@ -1,22 +1,19 @@
-import pandas as pd    
+def reader(dataset):
+	'''
+	Usage: 
+	from commonSenseQAReader import reader
+	from datasets import load_dataset
 
-def reader(file_path):
-	data_df = pd.read_json(path_or_buf=file_path, lines=True)
-	# sample datapoint
-	# {'question_concept': 'punishing', 'choices': [{'label': 'A', 'text': 'ignore'}, 
-	# {'label': 'B', 'text': 'enforce'}, {'label': 'C', 'text': 'authoritarian'}, {'label': 'D', 'text': 'yell at'}, {'label': 'E', 'text': 'avoid'}], 
-	# 'stem': 'The sanctions against the school were a punishing blow, and they seemed to what the efforts the school had made to change?'}
-	for idx in data_df.index:
-		ques = data_df["question"][idx]
-		ans = data_df["answerKey"][idx]
-		ques_str = ques['stem']
+	dataset = load_dataset("tau/commonsense_qa")
+	qa_reader = reader(dataset)
+	for ques, ans in qa_reader:
+		print(ques, ans)
+	'''
+	for data in dataset["train"]:
+		ques_str = data["question"]
+		ans = data["answerKey"]
 		ans_str = None
-		for choice in ques['choices']:
-			if(choice['label'] == ans):
-				ans_str = choice['text']
+		for text, label in zip(data['choices']['text'], data['choices']['label']):
+			if(label == ans):
+				ans_str = text
 		yield [ques_str, ans_str]
-
-
-out = reader("commonSenseQA/train_rand_split.jsonl")
-for ques, ans in out:
-	print(ques, ans)
